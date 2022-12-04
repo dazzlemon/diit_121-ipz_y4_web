dd99547a817f1eb2a61a12f946e14703625dd76b
dazzlemon <daanill13470@gmail.com>
2022-12-04 19:08:29 +0200

    almost working

---
+++ 4/website/src/index.js
@@ -0,0 +1,35 @@
+// preference
+const theme = localStorage.getItem('theme')
+
+if (theme === null && matchMedia?.('(prefers-color-scheme: dark)').matches) {
+	// initial use
+	localStorage.setItem('theme', 'dark')
+}
+
+const toggle = document.querySelector('#theme_toggle')
+toggle.addEventListener('click', themeSwitch)
+
+updateTheme()
+
+function themeSwitch() {
+	const theme = localStorage.getItem('theme')
+	localStorage.setItem('theme', theme === 'dark' ? 'light' : 'dark')
+	window.dispatchEvent(new Event('storage'))
+}
+
+function updateTheme() {
+	const theme = localStorage.getItem('theme')
+	toggle.innerText = theme === 'light' ? '🌚' : '🌞'
+	if (theme === 'light') {
+		document.body.classList.add('lightMode')
+	} else {
+		document.body.classList.remove('lightMode')
+	}
+}
+
+addEventListener('storage', updateTheme)
+
+const url = 'http://localhost:8080/'
+
+fetch(url)
+	.then(console.log)
